553890cd59d6256e6a2b14d7fc7a6a94843472c6
Diego Charles <diegocharles@me.com>
2021-06-21 16:24:25 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  password: 'Simple!123',
+  password_confirmation: 'Simple!123',
+  amount: 1000
+)
+
+Book.create!(
+  title: 'The ultimate code guide',
+  loan_fee: 4.99,
+  active_loans: 1,
+  units: 100
+)
